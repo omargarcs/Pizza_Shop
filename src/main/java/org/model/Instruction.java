001ff6d61dc4fc97e4806e6acf827a4261f73117
Omar Garc´s <omardario95@hotmail.com>
2020-05-10 12:02:22 -0500

    End creation Instruction Object.

--- src/main/java/org/model/Instruction.java
+++ src/main/java/org/model/Instruction.java
@@ -1,4 +1,23 @@
 package org.model;
 
 public class Instruction {
+
+    private String ingredient;
+    private int cookTime;
+
+    public String getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(String ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    public int getCookTime() {
+        return cookTime;
+    }
+
+    public void setCookTime(int cookTime) {
+        this.cookTime = cookTime;
+    }
 }
