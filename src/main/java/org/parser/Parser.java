e0417c64d56e9dd2c264e94859a19b0b86802577
Omar Garc´s <omardario95@hotmail.com>
2020-05-09 22:15:29 -0500

    Commit Branch - Parser
    Create method: readLine(), validateReadLine().

--- src/main/java/org/parser/Parser.java
+++ src/main/java/org/parser/Parser.java
@@ -1,4 +1,24 @@
 package org.parser;
 
 public class Parser {
+
+
+    public void readLine(String line) {
+
+        String[] parseLine = line.split(" ");
+
+    }
+
+    public boolean validateReadLine(String[] readLine) {
+
+        if (readLine.length != 2) {
+            return false;
+        }
+
+        String ingredient = readLine[0];
+        int timeCook = Integer.parseInt(readLine[1]);
+
+        return true;
+
+    }
 }
