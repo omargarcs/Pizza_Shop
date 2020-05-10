d5f2c0a35eb102c6119b50a5b9eaf0496cce0687
Omar Garc´s <omardario95@hotmail.com>
2020-05-10 13:25:04 -0500

    Integration of the "Instruction" Object with the "Parser" class.

--- src/main/java/org/parser/Parser.java
+++ src/main/java/org/parser/Parser.java
@@ -1,23 +1,35 @@
 package org.parser;
 
+import org.model.Instruction;
+
 public class Parser {
 
 
-    public void readLine(String line) {
+    public Instruction readLine(String line) {
 
         String[] parseLine = line.split(" ");
+
+        if (!validateReadLine(parseLine)) {
+            return null;
+        }
+
+        Instruction instruction = new Instruction();
+        instruction.setIngredient(parseLine[0]);
+        instruction.setCookTime(Integer.parseInt(parseLine[1]));
+
+        return instruction;
     }
 
     public boolean validateReadLine(String[] readLine) {
 
-        if (readLine.length != 2) {
-            return false;
+        if (readLine.length <= 2) {
+            return true;
         }
 
         //Only validate the Int value
         Integer.parseInt(readLine[1]);
 
-        return true;
+        return false;
 
     }
 }
