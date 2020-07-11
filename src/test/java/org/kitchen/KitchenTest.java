2cdcdc81a726dcbddb25925a0412037ff876cdda
Omar Garc´s <omardario95@hotmail.com>
2020-07-11 06:42:18 -0500

    Split TestCases

--- src/test/java/org/kitchen/KitchenTest.java
+++ src/test/java/org/kitchen/KitchenTest.java
@@ -12,20 +12,36 @@ public class KitchenTest {
         Instruction mexicana = new Instruction("ELOTE",15);
         Instruction hawuaiana = new Instruction("PINA",20);
         Instruction mariscos = new Instruction("ATUN",30);
-        Instruction quemada = new Instruction("ATUN",31);
-        Instruction cruda = new Instruction("ATUN",14);
-        Instruction noPizza = new Instruction("PASTOR",25);
 
         Assert.assertEquals("PIZZA_MEXICANA", kitchen.cookingInstruction(mexicana));
         Assert.assertEquals("PIZZA_HAWUAIANA", kitchen.cookingInstruction(hawuaiana));
         Assert.assertEquals("PIZZA_MARISCOS", kitchen.cookingInstruction(mariscos));
-        Assert.assertEquals("PIZZA_QUEMADA", kitchen.cookingInstruction(quemada));
-        Assert.assertEquals("PIZZA_CRUDA", kitchen.cookingInstruction(cruda));
-        Assert.assertEquals("ESO_NO_ES_PIZZA", kitchen.cookingInstruction(noPizza));
+    }
 
-        /* HOMEWORK
+    @Test
+    public void cookingInstructionThisIsNoPizzaTest(){
+        Kitchen kitchen = new Kitchen();
+        Instruction noPizza = new Instruction("PASTOR",25);
+        Assert.assertEquals("ESO_NO_ES_PIZZA", kitchen.cookingInstruction(noPizza));
+    }
+
+    @Test
+    public void cookingInstructionBurnPizzaTest(){
+        Kitchen kitchen = new Kitchen();
+        Instruction quemada = new Instruction("ATUN",31);
+        Assert.assertEquals("PIZZA_QUEMADA", kitchen.cookingInstruction(quemada));
+    }
+
+    @Test
+    public  void cookingInstructionRawPizza(){
+        Kitchen kitchen = new Kitchen();
+        Instruction cruda = new Instruction("ATUN",14);
+        Assert.assertEquals("PIZZA_CRUDA", kitchen.cookingInstruction(cruda));
+
+    }
+
+    /* HOMEWORK
         - Separar Pruebas
         - Cambiar if -> ValidateCook
         - Investigar y aplicar Enums */
-    }
 }
